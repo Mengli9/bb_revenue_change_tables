23bf7e844628a6c0dea5225c3a24d1ceac8bde56
Mengli9 <meng.li@sky.uk>
2023-12-12 17:24:24 +0000

    Updates for accounts base and filters

--- revenue-change-analysis/BB Revenue Change Analysis/bb_revenue_change_monthly.sql
+++ revenue-change-analysis/BB Revenue Change Analysis/bb_revenue_change_monthly.sql
@@ -1,4 +1,4 @@
-create temp table bb_base_monthly
+create or replace table `skyuk-uk-per-sub-economic-poc.ML.accounts_dates`
 as
 
 with dt_array as (
@@ -13,29 +13,88 @@ from        dt_array
 where       last_day(period_start,month) <= current_date()        
 )
 
-,accounts as (
+,accounts_bb as (
 select      distinct
             account_number
             ,portfolio_id
-from        dates dt
-inner join  `skyuk-uk-customer-pres-prod.uk_pub_cust_spine_subs_is.mart_subscription_broadband` msd
-on          dt.event_dt between cast(msd.effective_from_dt as date) and cast(msd.effective_to_dt as date)
-where       msd.status_code in ('AC', 'A', 'AB', 'PC','BCRQ', 'ACTIVE', 'PENDING_CEASE', 'BLOCKED')
-and 	      msd.sub_type in ('Broadband DSL Line')
+from        dates dt         
+inner join `skyuk-uk-per-sub-economic-poc.spine_replicas.mart_subscription_broadband` msd
+on          dt.event_dt < cast(msd.effective_to_dt as date)
+and         dt.event_dt >= cast(msd.effective_from_dt as date)
 and         date(msd.effective_to_dt) <> date(msd.effective_from_dt)
-and         currency_code = 'GBP'
+where       msd.sub_type in ('Broadband DSL Line') or ((lower(msd.sub_type) like '%boost%'
+or          upper(msd.sub_type) = 'WIFI_RELATED_SUBS'))
+and         msd.status_code in ('AC', 'AB', 'PC', 'PT', 'CF', 'BCRQ')
+and         msd.catalogue_product_type = 'RC'
+and         msd.currency_code = 'GBP'
 )
 
+,accounts_talk as (
+select      distinct
+            account_number
+            ,portfolio_id
+from        dates dt         
+inner join `skyuk-uk-per-sub-economic-poc.spine_replicas.mart_subscription_talk` mst
+on          dt.event_dt < cast(mst.effective_to_dt as date)
+and         dt.event_dt >= cast(mst.effective_from_dt as date)
+and         date(mst.effective_to_dt) <> date(mst.effective_from_dt)
+where       mst.status_code in ('RI', 'PR', 'PC', 'FBP', 'FBI', 'BCRQ', 'A', -- talk status codes
+                              'R', 'CRQ') -- lr status codes
+and         mst.catalogue_product_type = 'RC'
+and         mst.currency_code = 'GBP'
+)
+
+,accounts_all as (
+select *
+from accounts_bb
+union all
+select *
+from accounts_talk
+)
+
+,accounts as (
+select distinct account_number
+            ,portfolio_id
+from   accounts_all
+)
+
+, prep as (
 select      dates.*
             ,accounts.*
 from        accounts
 cross join  dates
+)
+
+select prep.*
+,case currency_code when 'GBP' then 'UK' when 'EURO' then 'ROI'
+                  else 'Unknown' end as country
+from prep
+left join   `skyuk-uk-customer-pres-prod.uk_pub_cust_spine_account_is.fact_account`
+using(account_number, portfolio_id)
 ;
 
 create or replace table `skyuk-uk-per-sub-economic-poc.Revenue_Change_Analysis.BB_Base_Monthly`
 as
 
-with bb_talk as (
+
+
+-- The changes are mainly added here.
+-- Change 1: CTE accounts_and_dates is adding the talk only and lr only customers
+-- Change 2: CTE holding all, add condtions for rack_rate and offers, updated the status_code we had in EVT.
+-- Change 3: In all CTE, change the join condtion using 'between' with '>='
+
+
+with accounts_and_dates as (
+      select *
+      from `skyuk-uk-per-sub-economic-poc.ML.bb_base_monthly_accounts_and_dates`
+--       where account_number in ('630091847826', '623941212614', '624421223899', '634242696835', '624272677706'  --bb
+-- , '624159043097', '210161408997', '624592873597', '622691874193', '620011284333' --boost
+-- , '210050647671', '624732891277', '624417407548', '622912631968', '623636182148' --talk
+-- ,'630215049788', '210054059378', '623805385605', '620021707125', '620022913516' -- lr
+)
+
+
+, bb_talk as (
 select     * 
 from        `skyuk-uk-customer-pres-prod.uk_pub_cust_spine_subs_is.mart_subscription_broadband`
 union all 
@@ -43,15 +102,11 @@ select      *
 from        `skyuk-uk-customer-pres-prod.uk_pub_cust_spine_subs_is.mart_subscription_talk`
 )
 
-,holdings_all as (
+, holdings_all as (
 select      a.account_number
             ,a.portfolio_id
             ,e.account_sub_type
             ,a.period_start
-            --evt
-            -- ,evt.x_alt_net_net_supplier_name
-            -- ,evt.fttp_active
-            -- ,mea.mobile_any_flag
             ,evt.customer_affluence
             ,evt.customer_income_band
             ,evt.customer_age_band
@@ -71,48 +126,115 @@ select      a.account_number
             ,f.bb_speed_category
             ,f.bb_tech_category
             ,f.speed as bb_speed
-            ,ifnull(case when upper(b.status_code) in ('AC', 'A', 'AB', 'PC','BCRQ', 'ACTIVE', 'PENDING_CEASE', 'BLOCKED', 'RI', 'PR', 'FBP', 'FBI', 'R', 'CRQ') then f.rate else 0 end,0) as or_rental
+            ,ifnull(case when upper(b.status_code) in ('AC', 'AB', 'PC', 'PT', 'CF', 'BCRQ','RI', 'PR', 'PC', 'FBP', 'FBI', 'A', 'R', 'CRQ')  then f.rate else 0 end,0) as or_rental
             -- breakdown
-            ,case       when b.sub_type = 'Broadband DSL Line'                                              then 'BB'
-                        when b.sub_type = 'BBBOOST' and b.catalogue_product_name ='Sky Broadband Boost'     then 'Boost'                           
-                        when b.sub_type = 'SKY TALK SELECT'                                                 then 'Talk'
-                        when b.sub_type = 'SKY TALK LINE RENTAL'                                            then 'LR'
+            -- Updated the defination for BB, Boost, Talk and LR
+            ,case       when b.status_code in ('AC', 'AB', 'PC', 'PT', 'CF', 'BCRQ')and b.sub_type = 'Broadband DSL Line' then 'BB'
+                        when b.status_code in ('AC', 'AB', 'PC', 'PT', 'CF', 'BCRQ') and (lower(b.sub_type) like '%boost%' or upper(b.sub_type) = 'WIFI_RELATED_SUBS') then 'Boost'                           
+                        when b.status_code in ('RI', 'PC', 'FBP', 'FBI', 'BCRQ', 'A') and (b.entitlement in (
+                         'Sky Pay As You Talk'      
+                        ,'Sky Pay As You Talk (VoIP)'                          
+                        ,'Sky Talk Evenings and Weekends Extra (Weekends)'
+                        ,'Sky Talk Evenings and Weekends Extra (Freetime)'
+                        ,'Sky Talk Evenings and Weekends Extra (VoIP)'
+                        ,'Sky Talk Evenings and Weekends Extra'
+                        ,'Sky Talk Anytime Extra (Anytime)'
+                        ,'Sky Talk Anytime Extra'
+                        ,'Sky Talk Anytime Extra (Anytime UK)'
+                        ,'Sky Talk Anytime Extra (24/7)'
+                        ,'Sky Talk Anytime Extra (VoIP)'
+                        ,'Sky Talk Anytime (VoIP)'
+                        ,'Sky Talk Extra'
+                        ,'Sky Talk Freetime'
+                        ,'Sky Talk Freetime (VoIP)'
+                        ,'Sky Talk Unlimited'
+                        ,'Sky Talk International Extra'
+                        ,'Sky Talk International Extra (Anytime International)'
+                        ,'Sky Talk International Extra (VoIP)'
+                        ,'Sky Talk International Extra (24/7 International)'
+                        ,'Sky Talk International Extra (Super Unlimited)'
+                        ,'Sky Talk Super Unlimited'
+                        ,'Sky Talk 24 / 7'
+                        ,'Sky Talk 24 / 7 + International'
+                     )
+                )  then 'Talk'
+                        when b.status_code in ('R', 'CRQ', 'BCRQ', 'A') and (replace(lower(b.sub_type), ' ', '_') like '%line_rental%')  then 'LR'
                         end as product
             -- contracts
             ,cast(c.created_dt as date) as contract_created_dt
-            ,case       when upper(b.status_code) in ('AC', 'A', 'AB', 'PC','BCRQ', 'ACTIVE', 'PENDING_CEASE', 'BLOCKED', 'RI', 'PR', 'FBP', 'FBI', 'R', 'CRQ')
+            ,case       when upper(b.status_code) in ('AC', 'AB', 'PC', 'PT', 'CF', 'BCRQ','RI', 'PR', 'PC', 'FBP', 'FBI', 'A', 'R', 'CRQ') 
                         then cast(c.start_dt as date) else null end as contract_start_date
-            ,case       when upper(b.status_code) in ('AC', 'A', 'AB', 'PC','BCRQ', 'ACTIVE', 'PENDING_CEASE', 'BLOCKED', 'RI', 'PR', 'FBP', 'FBI', 'R', 'CRQ')
+            ,case       when upper(b.status_code) in ('AC', 'AB', 'PC', 'PT', 'CF', 'BCRQ','RI', 'PR', 'PC', 'FBP', 'FBI', 'A', 'R', 'CRQ') 
                         then case   when cast(c.end_dt as date) is null then cast(c.calculated_minimum_term_end_dt as date)
                                     else cast(c.end_dt as date) end
                         else null end as contract_end_date
             ,case       when c.id is not null 
-                        and upper(b.status_code) in ('AC', 'A', 'AB', 'PC','BCRQ', 'ACTIVE', 'PENDING_CEASE', 'BLOCKED', 'RI', 'PR', 'FBP', 'FBI', 'R', 'CRQ')
+                        and upper(b.status_code) in ('AC', 'AB', 'PC', 'PT', 'CF', 'BCRQ','RI', 'PR', 'PC', 'FBP', 'FBI', 'A', 'R', 'CRQ') 
                         then 1 else 0 end as in_contract_flag   
             -- flags
-            ,case when upper(b.status_code) in ('AC', 'A', 'AB', 'PC','BCRQ', 'ACTIVE', 'PENDING_CEASE', 'BLOCKED', 'RI', 'PR', 'FBP', 'FBI', 'R', 'CRQ')then 1 else 0 end as active_flag
+            ,case when upper(b.status_code) in ('AC', 'AB', 'PC', 'PT', 'CF', 'BCRQ','RI', 'PR', 'PC', 'FBP', 'FBI', 'A', 'R', 'CRQ') then 1 else 0 end as active_flag
             ,case when upper(b.status_code) in ('PO', 'SC', 'TERMINATED', 'CEASED','CN') then 1 else 0 end as churned_flag
             -- rack rates
-            ,case when b.sub_type = 'SKY TALK SELECT' and lower(b.entitlement) like '%pay as you%' then 0
-                  when upper(b.status_code) in ('AC', 'A', 'AB', 'PC','BCRQ', 'ACTIVE', 'PENDING_CEASE', 'BLOCKED', 'RI', 'PR', 'FBP', 'FBI', 'R', 'CRQ') then coalesce(b.catalogue_product_price,0) 
+            ,case when b.status_code in ('R', 'CRQ', 'BCRQ', 'A') and (replace(lower(b.sub_type), ' ', '_') like '%line_rental%') then coalesce(b.catalogue_product_price,0) --lr
+                  when b.status_code in ('RI', 'PR', 'PC', 'FBP', 'FBI', 'BCRQ', 'A')
+                     and (b.entitlement in (
+                         'Sky Pay As You Talk'      
+                        ,'Sky Pay As You Talk (VoIP)'                          
+                        ,'Sky Talk Evenings and Weekends Extra (Weekends)'
+                        ,'Sky Talk Evenings and Weekends Extra (Freetime)'
+                        ,'Sky Talk Evenings and Weekends Extra (VoIP)'
+                        ,'Sky Talk Evenings and Weekends Extra'
+                        ,'Sky Talk Anytime Extra (Anytime)'
+                        ,'Sky Talk Anytime Extra'
+                        ,'Sky Talk Anytime Extra (Anytime UK)'
+                        ,'Sky Talk Anytime Extra (24/7)'
+                        ,'Sky Talk Anytime Extra (VoIP)'
+                        ,'Sky Talk Anytime (VoIP)'
+                        ,'Sky Talk Extra'
+                        ,'Sky Talk Freetime'
+                        ,'Sky Talk Freetime (VoIP)'
+                        ,'Sky Talk Unlimited'
+                        ,'Sky Talk International Extra'
+                        ,'Sky Talk International Extra (Anytime International)'
+                        ,'Sky Talk International Extra (VoIP)'
+                        ,'Sky Talk International Extra (24/7 International)'
+                        ,'Sky Talk International Extra (Super Unlimited)'
+                        ,'Sky Talk Super Unlimited'
+                        ,'Sky Talk 24 / 7'
+                        ,'Sky Talk 24 / 7 + International'
+                     )) then coalesce(b.catalogue_product_price,0) --talk
+                  when upper(b.status_code) in ('AC', 'AB', 'PC', 'PT', 'CF', 'BCRQ') and b.sub_type = 'Broadband DSL Line'  or (lower(b.sub_type) like '%boost%'or upper(b.sub_type) = 'WIFI_RELATED_SUBS') then coalesce(b.catalogue_product_price,0) --bb_boost
                   else 0 end as rack_rate          
-            --offers
-            ,sum(case   when g.offer_id is not null
-                        and upper(b.status_code) in ('AC', 'A', 'AB', 'PC','BCRQ', 'ACTIVE', 'PENDING_CEASE', 'BLOCKED', 'RI', 'PR', 'FBP', 'FBI', 'R', 'CRQ')
-                        then round(-coalesce(discount_value,0), 2) else 0 end) as tvfbbo
-            ,sum(case   when in_contract_offer_flag = True 
-                        and g.offer_id is null 
-                        and upper(b.status_code) in ('AC', 'A', 'AB', 'PC','BCRQ', 'ACTIVE', 'PENDING_CEASE', 'BLOCKED', 'RI', 'PR', 'FBP', 'FBI', 'R', 'CRQ')
-                        then round(-coalesce(discount_value,0), 2) else 0 end) as in_contract_discount
-            ,sum(case   when (in_contract_offer_flag != True or in_contract_offer_flag is null) 
+            -- offers
+            -- tvfbbo 
+            -- It will only be for BB, which means it will always be 0 for boost, talk and lr
+            ,sum( CASE WHEN (
+                lower(offer_bill_name) IN ('broadband subscription discounted', 'broadband subscription discount', 'tv discount with 6 months broadband unlimited offer')
+                AND (((SELECT COUNT(*) FROM `skyuk-uk-per-sub-economic-poc.rct02_uploads.tv_funded_bb_offers_upload` g WHERE g.description = offer_description) > 0 
+                  OR (lower(offer_description) LIKE '%tv%' OR lower(offer_description) LIKE '%stream%' OR lower(offer_description) LIKE '%glass%' OR lower(offer_description) LIKE '%signature%' OR lower(offer_description) LIKE '%ultimate%'))
+                AND lower(offer_description) NOT LIKE '%now%' AND lower(offer_description) NOT LIKE '%no tv%' AND lower(offer_description) NOT LIKE '%roi%'AND lower(offer_description) NOT LIKE '%not%')) THEN CASE WHEN g.offer_id IS NOT NULL THEN round(coalesce(-discount_value,0), 2) ELSE 0 END ELSE 0 END) as tvfbbo
+            -- ICD
+            -- It will only be for BB, which means it will always be 0 for boost, talk and lr
+            ,sum(-- bb
+                  case when upper(b.status_code) in ('AC', 'AB', 'PC', 'PT', 'CF', 'BCRQ') and b.sub_type = 'Broadband DSL Line'  
+                        and d.in_contract_offer_flag = true 
+                        and (lower(offer_description) like '%fibre%' or lower(offer_description) like '%broadband%'or lower(offer_description) like '%connect%'or lower(offer_description) like '%superfast%'or lower(offer_description) like '%ultrafast%' or lower(offer_description) like '%ftth%'or lower(offer_description) like '%essential%')
+                        and (lower(offer_description) not like '%super fibre%'and lower(offer_description) not like '%brilliant broadband%' and lower(offer_description) not like '%fab fibre%')
+                        and lower(offer_description) not like '%boost%'and lower(offer_description) not like '%sky broadband buddy%'
+                        and (lower(offer_bill_name) like '%contract%discount%' or lower(offer_bill_name) like '%broadband%')
+                        and coalesce(d.price_freeze_offer_flag, False) <> True then round(-coalesce(discount_value,0), 2)                  
+                        else 0 end)   as in_contract_discount
+            -- software_offer
+            ,sum(case   when (d.in_contract_offer_flag != True or d.in_contract_offer_flag is null) 
                         and g.offer_id is null
-                        and upper(b.status_code) in ('AC', 'A', 'AB', 'PC','BCRQ', 'ACTIVE', 'PENDING_CEASE', 'BLOCKED', 'RI', 'PR', 'FBP', 'FBI', 'R', 'CRQ')
+                        and upper(b.status_code) in ('AC', 'AB', 'PC', 'PT', 'CF', 'BCRQ','RI', 'PR', 'PC', 'FBP', 'FBI', 'A', 'R', 'CRQ') 
                         then round(-coalesce(discount_value,0), 2) else 0 end) as software_offer
-from        bb_base_monthly a
+from        accounts_and_dates a
 left join   bb_talk b
 on          a.portfolio_id = b.portfolio_id
 and         a.account_number = b.account_number
-and         a.event_dt between cast(b.effective_from_dt as date) and cast(b.effective_to_dt as date)
+and         a.event_dt < cast(b.effective_to_dt as date)
+and         a.event_dt >= cast(b.effective_from_dt as date)
 and         date(b.effective_to_dt) <> date(b.effective_from_dt)
 left join   `skyuk-uk-customer-pres-prod.uk_pub_cust_spine_subs_is.dim_subscription_agreement_item` c
 on          b.subscription_id = c.subscription_id
@@ -141,14 +263,14 @@ on          d.offer_detail_id = cast(g.offer_id as string)
 left join   `skyuk-uk-per-sub-economic-poc.Per_Sub_Economics_Finance.New_Everything_Comcast_Monthly` evt
 on          a.account_number = evt.account_number
 and         a.event_dt = evt.event_dt
--- left join   `skyuk-uk-per-sub-economic-poc.Mobile_Everything_Tables.Mobile_Everything_Acc` mea
--- on          a.portfolio_id = mea.portfolio_id
--- and         a.event_dt = mea.event_dt
--- and         date_sub(a.event_dt, interval 1 day) = mea.event_dt
+-- Added from EVT
+where b.catalogue_product_type = 'RC'
+and     (d.active_flag)
+and      d.offer_transaction_type = 'RC'
+
 group by    a.account_number
             ,a.portfolio_id
             ,e.account_sub_type
-            -- ,mobile_any_flag
             ,customer_affluence
             ,customer_income_band
             ,customer_age_band
@@ -180,7 +302,7 @@ group by    a.account_number
 qualify     rank () over     (partition by account_number
                                           ,event_dt
                                           ,product
-                              order by    case when product = 'Talk' and upper(status_code) in ('AC', 'A', 'AB', 'PC','BCRQ', 'ACTIVE', 'PENDING_CEASE', 'BLOCKED', 'RI', 'PR', 'FBP', 'FBI', 'R', 'CRQ') then 0 else 1 end
+                              order by    case when product = 'Talk' and upper(status_code) in ('AC', 'AB', 'PC', 'PT', 'CF', 'BCRQ','RI', 'PR', 'PC', 'FBP', 'FBI', 'A', 'R', 'CRQ')  then 0 else 1 end
                                           ,subscription_created_dt desc
                                           ,history_order desc) = 1 
 and         rank () over     (partition by account_number
@@ -198,7 +320,7 @@ select      *
             ,lag(entitlement) over (partition by account_number, product order by event_dt) as prev_entitlement
 from        holdings_all
 where       product is not null
-and         case when product = 'Talk' and rack_rate = 0 and upper(status_code) in ('AC', 'A', 'AB', 'PC','BCRQ', 'ACTIVE', 'PENDING_CEASE', 'BLOCKED', 'RI', 'PR', 'FBP', 'FBI', 'R', 'CRQ')
+and         case when product = 'Talk' and rack_rate = 0 and upper(status_code) in ('AC', 'AB', 'PC', 'PT', 'CF', 'BCRQ','RI', 'PR', 'PC', 'FBP', 'FBI', 'A', 'R', 'CRQ') 
             then 0 else 1 end = 1
 ;
 
